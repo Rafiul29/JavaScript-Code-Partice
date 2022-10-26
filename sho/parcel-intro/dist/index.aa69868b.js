a87781abb6b2d4d89ba19cb5f4f3b339be2b6403
Rafiul29 <rafiulislam4472@gmail.com>
2022-10-27 01:44:22 +0600

    AJAX

---
+++ sho/parcel-intro/dist/index.aa69868b.js
@@ -0,0 +1,28 @@
+// AJAX - Asynchronous javaScript and XML
+const wrapper = document.querySelector(".country-wrapper");
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", function() {
+    const input = document.querySelector(".inputcountry");
+    const inputVale = input.value;
+    const resquest = new XMLHttpRequest();
+    resquest.open("GET", `https://restcountries.com/v2/name/${inputVale}`);
+    resquest.send();
+    resquest.addEventListener("load", function() {
+        const [data] = JSON.parse(this.responseText);
+        renderData(data);
+    });
+});
+function renderData(country) {
+    console.log(country);
+    const html = `
+    <div class="country">
+                <h1>${country.name}</h1>
+                <p>${country.population}</p>
+                <p>${country.capital}</p>
+                <p></p>
+            </div>
+    `;
+    wrapper.insertAdjacentHTML("afterbegin", html);
+}
+
+//# sourceMappingURL=index.aa69868b.js.map
--- sho/parcel-intro/src/js/main.js
+++ sho/parcel-intro/src/js/main.js
@@ -1,2 +1,34 @@
-const rafiul =555;
-console.log(rafiul)
+// AJAX - Asynchronous javaScript and XML
+
+const wrapper =document.querySelector('.country-wrapper')
+
+const btn= document.querySelector('.btn');
+
+
+btn.addEventListener('click',function(){
+    const input=document.querySelector('.inputcountry')
+const inputVale = input.value;
+    const resquest= new XMLHttpRequest()
+    resquest.open('GET',`https://restcountries.com/v2/name/${inputVale}`)
+    resquest.send()
+    
+    resquest.addEventListener('load',function(){
+        const [data] = JSON.parse(this.responseText)
+        renderData(data)
+    })
+    
+})
+
+function renderData(country){
+    console.log(country)
+
+    const html=`
+    <div class="country">
+                <h1>${country.name}</h1>
+                <p>${country.population}</p>
+                <p>${country.capital}</p>
+                <p></p>
+            </div>
+    `;
+    wrapper.insertAdjacentHTML('afterbegin',html);
+}
